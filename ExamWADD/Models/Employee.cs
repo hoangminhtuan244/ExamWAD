e33f3feb3a0e39b611899babdc403ec1bf41c703
minhtuan244 <hoangminhtuanfpt992@gmail.com>
2020-03-17 21:21:32 +0700

    Nop exam

---
+++ ExamWADD/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExamWADD.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
